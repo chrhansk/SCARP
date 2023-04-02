536b4d2ee20b6f2a9787cd40d440806be3bbbb8c
Christoph Hansknecht <c.hansknecht@tu-braunschweig.de>
2023-04-02 20:10:14 +0200

    Add SUR cost test

--- src/test/scarp/scarp_program_test.cc
+++ src/test/scarp/scarp_program_test.cc
@@ -1,5 +1,7 @@
 #include "scarp_program_test.hh"
 
+#include "cost_function.hh"
+
 double SCARPProgramTest::execute(const Instance& instance,
                                  const CostFunction& costs,
                                  bool vanishing_constraints)
@@ -13,6 +15,15 @@ double SCARPProgramTest::execute(const Instance& instance,
                        vanishing_constraints);
 
   auto controls = program.solve();
+
+  const double distance = controls.distance(fractional_controls,
+                                            instance.get_mesh());
+
+  SURCosts sur_costs(instance);
+
+  const double sur_cost = sur_costs.total_cost(controls, fractional_controls);
+
+  assert(cmp::eq(distance, sur_cost));
 
   return costs.total_cost(controls, fractional_controls);
 }
