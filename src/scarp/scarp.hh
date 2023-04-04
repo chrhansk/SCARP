729be466b67fdc821794efd263ba5a163af96bcf
Christoph Hansknecht <c.hansknecht@tu-braunschweig.de>
2023-04-04 10:09:30 +0200

    Add global header

---
+++ src/scarp/scarp.hh
@@ -0,0 +1,24 @@
+#ifndef SCARP_HH
+#define SCARP_HH
+
+#include "scarp/dwt/dwt_label.hh"
+#include "scarp/dwt/dwt_program.hh"
+#include "scarp/scarp/scarp_label.hh"
+#include "scarp/scarp/scarp_program.hh"
+#include "scarp/sur/sur.hh"
+#include "scarp/ansi_color.hh"
+#include "scarp/bounded_label_set.hh"
+#include "scarp/cmp.hh"
+#include "scarp/control_writer.hh"
+#include "scarp/parameters.hh"
+#include "scarp/util.hh"
+#include "scarp/cost_function.hh"
+#include "scarp/instance.hh"
+#include "scarp/instance_reader.hh"
+#include "scarp/label.hh"
+#include "scarp/matrix.hh"
+#include "scarp/mesh.hh"
+#include "scarp/controls.hh"
+#include "scarp/handle.hh"
+
+#endif /* SCARP_HH */
